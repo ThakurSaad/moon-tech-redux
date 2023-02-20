132320a0d39df404d2497732ad19a7143be34a58
ThakurSaad <thakursaad613@gmail.com>
2023-02-20 22:56:44 +0600

    reomve form cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/actionCreators/actionCreators";
+import {
+  addToCart,
+  removeFromCart,
+} from "../redux/actionCreators/actionCreators";
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
@@ -34,6 +37,20 @@ const ProductCard = ({ product }) => {
         <button
           title="Add to wishlist"
           className="bg-indigo-500  py-1 px-2 rounded-full"
+        >
+          <BiListPlus className="text-white" />
+        </button>
+      </div>
+      <div className="flex gap-2 mt-5">
+        <button
+          className="bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          onClick={() => dispatch(removeFromCart(product))}
+        >
+          Remove From Cart
+        </button>
+        <button
+          title="Remove from wishlist"
+          className="bg-red-500  py-1 px-2 rounded-full"
         >
           <BiListPlus className="text-white" />
         </button>
