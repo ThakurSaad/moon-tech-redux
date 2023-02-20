de872d7a0804c05a757555e12d17b05b34cf9698
ThakurSaad <thakursaad613@gmail.com>
2023-02-20 23:04:55 +0600

    show butttons based on path

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { BiListPlus } from "react-icons/bi";
+import { BiListMinus, BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 import {
   addToCart,
   removeFromCart,
@@ -8,6 +9,7 @@ import {
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
 
   return (
     <div className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900">
@@ -28,32 +30,40 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className="flex gap-2 mt-5">
-        <button
-          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-          onClick={() => dispatch(addToCart(product))}
-        >
-          Add to cart
-        </button>
-        <button
-          title="Add to wishlist"
-          className="bg-indigo-500  py-1 px-2 rounded-full"
-        >
-          <BiListPlus className="text-white" />
-        </button>
+        {!pathname.includes("cart") && (
+          <button
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+            onClick={() => dispatch(addToCart(product))}
+          >
+            Add to cart
+          </button>
+        )}
+        {!pathname.includes("cart") && (
+          <button
+            title="Add to wishlist"
+            className="bg-indigo-500  py-1 px-2 rounded-full"
+          >
+            <BiListPlus className="text-white" />
+          </button>
+        )}
       </div>
       <div className="flex gap-2 mt-5">
-        <button
-          className="bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-          onClick={() => dispatch(removeFromCart(product))}
-        >
-          Remove From Cart
-        </button>
-        <button
-          title="Remove from wishlist"
-          className="bg-red-500  py-1 px-2 rounded-full"
-        >
-          <BiListPlus className="text-white" />
-        </button>
+        {pathname.includes("cart") && (
+          <button
+            className="bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+            onClick={() => dispatch(removeFromCart(product))}
+          >
+            Remove From Cart
+          </button>
+        )}
+        {pathname.includes("wishlist") && (
+          <button
+            title="Remove from wishlist"
+            className="bg-red-500  py-1 px-2 rounded-full"
+          >
+            <BiListMinus className="text-white" />
+          </button>
+        )}
       </div>
     </div>
   );
