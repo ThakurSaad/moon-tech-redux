f238418074228bee6bbf2de8d6c0d205b633e23e
ThakurSaad <thakursaad613@gmail.com>
2023-02-21 15:40:10 +0600

    module 3 done

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,6 @@ import productReducer from "./reducers/productReducer";
 // import { legacy_createStore as createStore} from 'redux'
 import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(productReducer, composeWithDevTools());
+const store = createStore(productReducer, composeWithDevTools()); // using redux-devtool
 
 export default store;
