326d72c80307632854199ce491948856aa631451
ThakurSaad <thakursaad613@gmail.com>
2023-02-21 14:05:56 +0600

    module 3.7

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -12,7 +12,12 @@ const ProductCard = ({ product }) => {
   const { pathname } = useLocation();
 
   return (
-    <div className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900">
+    <div className="relative shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900">
+      {pathname.includes("cart") && (
+        <div className="grid place-items-center absolute top-2 right-2 bg-indigo-500 rounded-full text-white h-8 w-8">
+          <p>{product.quantity}</p>
+        </div>
+      )}
       <div className="h-52 w-52 mx-auto">
         <img src={product.image} alt={product.model} />
       </div>
